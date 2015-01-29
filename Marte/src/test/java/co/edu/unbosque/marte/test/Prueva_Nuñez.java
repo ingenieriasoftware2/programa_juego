0067ddd071b1f294e1aace97421a38066631070f
pablo <pablo@pablo-HP>
2015-01-29 14:53:40 -0500

    lectura de txt

--- Marte/src/test/java/co/edu/unbosque/marte/test/Prueva_Nuñez.java
+++ Marte/src/test/java/co/edu/unbosque/marte/test/Prueva_Nuñez.java
@@ -6,10 +6,34 @@
 
 package co.edu.unbosque.marte.test;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import org.junit.Test;
+
 /**
  *
  * @author pablo
  */
 public class Prueva_Nuñez {
-    
+ @ Test   
+public String leerTxt(){
+    String direccion="C:\\Users\\pablo\\Desktop\\juego.tx";
+    String texto ="";
+       
+    try {
+        BufferedReader bf = new BufferedReader(new FileReader(direccion));
+        String temp ="";
+        String bfRead;
+        while((bfRead = bf.readLine()) != null){
+            temp = temp + bfRead;
+        }
+        texto=temp;
+    } catch (IOException e) {
+        System.err.println("No se encontro archivo");
+        
+    }
+    return texto;
+}
 }
